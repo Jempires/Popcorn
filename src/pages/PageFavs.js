83e4ef5512ac449231dbe593481d7568741e26e5
katy-chen <118566165+katy-chen@users.noreply.github.com>
2023-02-04 13:08:55 -0800

    Add imports to PageFavs.js

--- src/pages/PageFavs.js
+++ src/pages/PageFavs.js
@@ -1,19 +1,5 @@
-import React from "react";
-import admitOne from '../components/images/admitOne.jpg';
-import { Link } from "react-router-dom";
-
-//import Nav from '../components/Nav'
+// Imports
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux'; 
 
 
-function Landing({title}) {
-
-    return (
-        <main className="landing-page">
-            <h1>{title}</h1>
-           
-         
-        </main>
-    );
-}
-
-export default Landing;
