6ae29ddfae4b9c53bc463211ec5a4451bae6cfd2
Jennifer Rafter <77985983+Jempires@users.noreply.github.com>
2023-02-03 17:15:56 -0800

    new message

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -8,6 +8,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "../components/Header";
 //import Footer from "../components/Footer";
 
+//practice comment
 
 // Pages
 import PageHome from "../pages/PageHome";
