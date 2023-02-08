7c8934f5cdc5543e3f2fdb95e6347e68eeefee06
katy-chen <118566165+katy-chen@users.noreply.github.com>
2023-02-08 14:22:08 -0800

    Update store import on index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import AppRouter from './routers/AppRouter';
 import reportWebVitals from './reportWebVitals';
 import "./styles/App.css";
-import { store } from 'react-redux';
+import { store } from './store/store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
