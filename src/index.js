951eab3272771b6599b449393787d05f3aca78d4
katy-chen <118566165+katy-chen@users.noreply.github.com>
2023-02-08 14:21:27 -0800

    Updated index.js store set up

--- src/index.js
+++ src/index.js
@@ -9,9 +9,12 @@ import { store } from 'react-redux';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <AppRouter />
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
   </React.StrictMode>
 );
 
