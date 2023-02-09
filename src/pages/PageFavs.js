ffb3fd764bcf410e34444558ecaddfe57a4b3c7b
katy-chen <118566165+katy-chen@users.noreply.github.com>
2023-02-08 21:57:04 -0800

    Add functionality to Favorites page

--- src/pages/PageFavs.js
+++ src/pages/PageFavs.js
@@ -1,12 +1,32 @@
-// // Imports
-// import { Link } from 'react-router-dom';
-// import { useSelector } from 'react-redux'; 
+// Imports
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux'; 
+import MovieCard from '../components/MovieCard';
 // import { useState } from 'react';
 
-// const PageFavs () => {
+function PageFavs () {
 
-//     const [favorites, setFavorites] = useSelector
+    const favorites = useSelector((state) => state.favorites.favItems);
+    
+    return (
+        <section className="page-favs">
+            <h2>Favorites Movies</h2>
+            {favorites.length < 1 ? 
+            <div className="error-msg">
+                <p>Sorry! There are currently no movies on your favorites list.</p>
+                <br></br>
+                <p>You may browse through movies on the home page and add movies to your list.</p>
+            </div> : 
+                <div id='movie-card'>
+                    {favorites.map((movie)=> {
+                        return <MovieCard key={movie.id} movie={movie} />
+                    }
+                    )}
+                </div>
+            };
 
-// };
+        </section>
+    );
+};
 
-// export default PageFavs;
+export default PageFavs;
