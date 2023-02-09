c7aa35e0fb866de1b30c4223da8ded2994bdbb2e
katy-chen <118566165+katy-chen@users.noreply.github.com>
2023-02-08 21:58:41 -0800

    Update Fav Button component

--- src/components/FavButton.js
+++ src/components/FavButton.js
@@ -5,11 +5,13 @@ import { addToFavorites, deleteFromFavorites } from '../features/favorites/favSl
 const FavButton = ({favs, singleMovie }) => {
 
     // favorites and items need to be same as what is in the store and slice components 
-    const favArray = useSelector((state) => state.favorites.items);
+    const favArray = useSelector((state) => state.favorites.favItems);
+
     // dispatch calls in add and remove (redux function)
     const dispatch = useDispatch();
 
     const handleClick = (e, type) => {
+        e.preventDefault();
         
         if(type === 'delete') {
             // DISPATCHING FROM SLICE.targeting the item from the array
@@ -18,16 +20,28 @@ const FavButton = ({favs, singleMovie }) => {
             dispatch(addToFavorites(singleMovie));
         }
     };
+    console.log(favArray);
 
     return(
         // Empty div, helps with styling so you're not limited to the divs. helps contain return 
         <>
+        {console.log(favArray)}
         {/* Validates if the favorites movie is already in the fav array. Helps grab correct class */}
         {/* if index is greater than -1, it meansit exists so show remove/add icon */}
+        {/* ? is error checking if array is null */}
         {favArray.findIndex(arrayObject => arrayObject.id === singleMovie.id) > -1 ? 
-        <heartIcon className="delFav" onClick={(e) => handleClick(e, 'delete')} /> :
-        // Add favorite
-        <heartIcon className="addFav" onClick={(e) => handleClick(e, 'add')} /> 
+
+        <button className='delFav' onClick={(e) => handleClick(e, 'delete')} >
+            Remove
+        </button> :
+        <button className='addFav' onClick={(e) => handleClick(e, 'add')} >
+            Add
+        </button> 
+
+        // delete fav
+        // <img src={heartIcon} alt='heart icon' className="delFav" onClick={(e) => handleClick(e, 'delete')} /> :
+        // // Add favorite
+        // <img src={heartIcon} alt='heart icon' className="addFav" onClick={(e) => handleClick(e, 'add')} /> 
         }
         
         </>
