25cc6fe66fbbbe42bdc6e135bd569066a6a32000
katy-chen <118566165+katy-chen@users.noreply.github.com>
2023-02-08 16:01:49 -0800

    Delete web vitals report and added store to index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppRouter from './routers/AppRouter';
-import reportWebVitals from './reportWebVitals';
 import "./styles/App.css";
-import { store } from './store/store';
+import { store } from './stores/store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,7 +17,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
