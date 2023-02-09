d2565895d6514a77906c19276acab355980ea52e
katy-chen <118566165+katy-chen@users.noreply.github.com>
2023-02-08 21:55:50 -0800

    Update More Info component

--- src/pages/PageMoreInfo.js
+++ src/pages/PageMoreInfo.js
@@ -1,57 +1,57 @@
-//PageMoreInfo.js
+// //PageMoreInfo.js
 
-import React from 'react';
+// import React from 'react';
+
+// // import { useState, useEffect } from 'react';
+// // import { useParams } from 'react-router-dom';
+// // import { BASE_URL, API_KEY,} from '../globals/globals';
+// // import PageHome from "../pages/PageHome";
+
+// const PageMoreInfo = (props) => {
+//     const movie = props.location.state.movie;
+//     return (
+//         <div className="">
+//             <h1>{movie.title}</h1>
+//             <p>{movie.overview}</p>
+//         </div>
+//     )
+// }
+   
+// export default SingleMovie;
 
 // import { useState, useEffect } from 'react';
 // import { useParams } from 'react-router-dom';
-// import { BASE_URL, API_KEY,} from '../globals/globals';
-// import PageHome from "../pages/PageHome";
+// import { BASE_URL, API_KEY, IMG_PATH} from '../globals/globals';
+// // import PageHome from "../pages/PageHome";
 
-const SingleMovie = (props) => {
-    const movie = props.location.state.movie;
-    return (
-        <div className="">
-            <h1>{movie.title}</h1>
-            <p>{movie.overview}</p>
-        </div>
-    )
-}
+// const SingleMovie = () => {
+//     const [movieInfo, setMovieInfo] = useState({});
+//     const [moviePoster, setMoviePoster] = useState({});
    
-export default SingleMovie;
+//     let {id} = useParams();
+//     id = id * 1; 
 
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { BASE_URL, API_KEY, IMG_PATH} from '../globals/globals';
-// import PageHome from "../pages/PageHome";
 
-const SingleMovie = () => {
-    const [movieInfo, setMovieInfo] = useState({});
-    const [moviePoster, setMoviePoster] = useState({});
+// // const [movie, setMovie] = useState({});
    
-    let {id} = useParams();
-    id = id * 1; 
+// // let {id} =useParams();
+
+// // // fetching movie details
+// // useEffect(() => {
+// //     const fetchMovie = async() => {
+// //         const fetchedResult = await fetch(`${BASE_URL}/${id}?api_key=${API_KEY}`);
+// //         let data = await fetchedResult.json();
+// //         setMovie(data);
+// //     };
+// //     fetchMovie();
+// // },[id]);
 
 
-// const [movie, setMovie] = useState({});
-   
-// let {id} =useParams();
+// // return(
+// //     <div>
+// //         <h1>{movie.title}</h1>
+// //         <p>{movie.overview}</p>
+// //     </div>
+// // )
 
-// // fetching movie details
-// useEffect(() => {
-//     const fetchMovie = async() => {
-//         const fetchedResult = await fetch(`${BASE_URL}/${id}?api_key=${API_KEY}`);
-//         let data = await fetchedResult.json();
-//         setMovie(data);
-//     };
-//     fetchMovie();
-// },[id]);
-
-
-// return(
-//     <div>
-//         <h1>{movie.title}</h1>
-//         <p>{movie.overview}</p>
-//     </div>
-// )
-
-// }
+// // }
