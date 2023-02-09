1724cf711c072954154e9577e5bb7b608b5295d0
Jennifer Rafter <77985983+Jempires@users.noreply.github.com>
2023-02-08 23:24:06 -0800

    css for favs page and merge clean up

--- src/pages/PageFavs.js
+++ src/pages/PageFavs.js
@@ -1,5 +1,5 @@
 // Imports
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux'; 
 import MovieCard from '../components/MovieCard';
 // import { useState } from 'react';
@@ -10,12 +10,12 @@ function PageFavs () {
     
     return (
         <section className="page-favs">
-            <h2>Favorites Movies</h2>
+            <h2 className='favs-title'>Favorites Movies</h2>
             {favorites.length < 1 ? 
             <div className="error-msg">
-                <p>Sorry! There are currently no movies on your favorites list.</p>
-                <br></br>
-                <p>You may browse through movies on the home page and add movies to your list.</p>
+                <p className='favs-error'>Sorry! There are currently no movies on your favorites list.
+
+                You may browse through movies on the home page and add movies to your list.</p>
             </div> : 
                 <div id='movie-card'>
                     {favorites.map((movie)=> {
