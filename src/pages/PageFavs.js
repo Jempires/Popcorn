48f5ae01ba5122727c87692cefa064d0f766e0bd
Jennifer Rafter <77985983+Jempires@users.noreply.github.com>
2023-03-01 13:04:26 -0800

    added ? to ids

--- src/pages/PageFavs.js
+++ src/pages/PageFavs.js
@@ -21,15 +21,15 @@ const PageFavs = () => {
         <>
         <section className="page-favs">
             <h2 className='favs-title'>My Favorites</h2>
-            {favorites.length < 1 ?
+            {favorites?.length < 1 ?
             <div className="error-msg">
                 <p className='favs-error'>Sorry! You currently have no favorites.<br></br>
 
                 You may browse through movies on the home page and click the <img  src={heart} alt=''/> to add movies to your list.</p>
             </div> : 
                 <div id='movie-card'>
-                    {favorites.map((movie)=> {
-                        return <MovieCard key={movie.id} movie={movie} />
+                    {favorites?.map((movie)=> {
+                        return <MovieCard key={movie?.id} movie={movie} />
                     }
                     )}
                 </div>
